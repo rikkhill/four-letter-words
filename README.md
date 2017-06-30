038d047f0d316a7cdaa6ec3869c3aefb1647c2a3
Rikk Hill <rikk@unashamedly.co.uk>
2017-06-30 16:20:55 +0100

    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # four-letter-words
 An anomaly-detecting autoencoder for detecting fake four-letter words
+
+Autoencoders use a neural network which takes an input, passes the  
+activation through a low-dimensional "bottleneck" layer,  
+and then tries to reconstruct the original input from this sparse 
+low-dimensional representation.
+
+This is commonly used for dimensionality reduction, but it also  
+poses another interesting use-case: anomaly detection. Autoencoders  
+are trained by minimising the reconstruction error between the  
+input and output, so by training the network on a well-represented  
+set of inputs, the reconstruction error on the trained network  
+should be higher for atypical or unusual examples.
+
+This autoencoder is trained on a corpus of (real, intelligible)  
+four-letter words. The intention is that it should provide a low  
+reconstruction error for "real-looking" four-letter words  
+(including actually real words like "ball" or "fish", but also  
+words with a similar construction to natural-sounding  four-letter  
+words such as "pone" or "woog"), while providing a high  
+reconstruction error for words which are not real or  
+natural-sounding (such as "jqxo" or "wwov").
---
+++ train_convnet.py
@@ -0,0 +1,81 @@
+from keras.layers import Input, Dense
+from keras.models import Model
+import numpy as np
+import random
+
+from helpers import filereader, text
+
+##############
+# ETL process to get word vectors
+
+# Load in all words
+words = filereader.get_unix_dict() + filereader.get_scrabble_dict()
+
+# Case-insensitive
+words = [w.lower() for w in words]
+
+# De-diacriticise
+words = [text.romanify(w) for w in words]
+
+# Deduplicate
+words = list(set(words))
+
+# Vectorise
+vectors = [text.vectorise(w) for w in words]
+
+# Shuffle
+random.shuffle(vectors)
+
+# Matrixify
+
+# Split into test/train
+
+train = np.asarray(vectors[:int((len(vectors)+1)*.80)])
+test = np.asarray(vectors[int(len(vectors) * .80 + 1):])
+
+print(train.shape)
+print(test.shape)
+
+
+##############
+# The autoencoder
+
+
+input_word = Input(shape=(104, ))
+
+encoded = Dense(26, activation='relu')(input_word)
+encoded = Dense(12, activation='relu')(encoded)
+encoded = Dense(6, activation='relu')(encoded)
+decoded = Dense(12, activation='relu')(encoded)
+decoded = Dense(26, activation='relu')(decoded)
+decoded = Dense(104, activation='sigmoid')(decoded)
+
+
+autoencoder = Model(input_word, decoded)
+
+#encoder = Model(input_word, encoded)
+
+#encoded_input = Input(shape=(13, ))
+#decoder_layer = autoencoder.layers[-1]
+#decoder = Model(encoded_input, decoder_layer(encoded_input))
+
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+autoencoder.fit(train, train,
+                epochs=5000,
+                batch_size=16,
+                shuffle=True,
+                validation_data=(test, test),
+                verbose=2)
+
+
+words = ["crud", "ball", "woob", "grue", "xbhq", "lakg", "lcfw"]
+
+
+for w in words:
+    wordvec = np.asarray([text.vectorise(w)])
+    print(w)
+    print(autoencoder.evaluate(wordvec, wordvec, 1))
+
+
+autoencoder.save("./models/flw_auto.h5")
